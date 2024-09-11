b49a4567e173b63ed9261011fe9234cda750a66e
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-09-11 14:39:32 -0700

    Finishing day3-part-1 that speaks about component life cycle. and we solve the problem of defaultColor parameter in the lightBox directive using OnChanges interface or OnInit interfce and the two ways works well

--- src/app/Directives/light-box.directive.ts
+++ src/app/Directives/light-box.directive.ts
@@ -1,16 +1,24 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[LightBox]'
 })
-export class LightBoxDirective {
+// export class LightBoxDirective implements OnInit{
+export class LightBoxDirective implements OnChanges{
 
   @Input('LightBox') highLightColor:string="yellow";
   @Input() defaultColor:string = "darkblue";
 
   constructor(private elemRef:ElementRef)
   {
+    // this.elemRef.nativeElement.style.border=`2px solid ${this.defaultColor}`;
+  }
+  // ngOnInit(): void {
+  //   this.elemRef.nativeElement.style.border=`2px solid ${this.defaultColor}`;
+  // }
+  ngOnChanges(): void {
     this.elemRef.nativeElement.style.border=`2px solid ${this.defaultColor}`;
+
   }
 
   @HostListener('mouseover') onMouseOver()
