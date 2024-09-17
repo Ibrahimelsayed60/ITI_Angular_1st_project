fa42d9f6f780f43bd11fd30c4a196086fafdaaab
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-09-17 10:48:25 -0700

    Finishing component interaction - @viewchild

--- src/app/Components/Order/order-master/order-master.component.ts
+++ src/app/Components/Order/order-master/order-master.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ICategory } from 'src/app/Models/icategory';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 @Component({
   selector: 'app-order-master',
   templateUrl: './order-master.component.html',
   styleUrls: ['./order-master.component.scss']
 })
-export class OrderMasterComponent implements OnInit {
+export class OrderMasterComponent implements OnInit, AfterViewInit {
 
   catList:ICategory[];
   selectedCatID:number=0;
   receivedOrderTotalPrice:number=0;
+
+  // clientNameInpElem:ElementRef = new ElementRef();
+  // clientNameInpElem:ElementRef = {} as ElementRef;
+  // clientNameInpElem?:ElementRef; // optional
+  // clientNameInpElem:ElementRef | undefined = undefined;
+  // clientNameInpElem:ElementRef | null = null;
+  @ViewChild('clientNameInp') clientNameInpElem!:ElementRef; // Non-null asseration operator
+  @ViewChild(ProductListComponent) prdListCompObj!: ProductListComponent;
   constructor()
   {
     this.catList = [
@@ -19,6 +28,13 @@ export class OrderMasterComponent implements OnInit {
       {id: 3, name:"Mobile"},
     ];
   }
+  ngAfterViewInit(): void {
+    this.clientNameInpElem.nativeElement.value = "Your name here";
+    this.clientNameInpElem.nativeElement.style.border = "2px solid red";
+
+    console.log(this.prdListCompObj.prdList);
+
+  }
 
   ngOnInit(): void {
   }
@@ -26,6 +42,11 @@ export class OrderMasterComponent implements OnInit {
   onTotalPriceChanged(totalPrice:number)
   {
     this.receivedOrderTotalPrice = totalPrice;
+  }
+
+  completeOrder()
+  {
+    this.prdListCompObj.prdList[0].quantity -= 1;
   }
 
 }
